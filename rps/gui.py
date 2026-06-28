180dd02dd9f05fda73d011a99d09859a64b56d71
Carson Irving <carsoni@alumni.iastate.edu>
2026-06-28 13:10:53 -0500

    Basic Game Logic

--- rps/gui.py
+++ rps/gui.py
@@ -8,11 +8,27 @@ from PyQt6.QtWidgets import (
 
 class RockPaperScissors(QMainWindow):
     def __init__(self):
-        #QT Init
+        # QT Init
         super().__init__() 
 
 
         self.setWindowTitle("Rock Paper Scissors :)")
         self.setFixedSize(400, 400)
+        
+        #Scoring
+        self.player_score = 0
+        self.computer_score = 0
+
+        self.title_label = QLabel("Rock Paper Scissors")
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.title_label)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+
         
 
